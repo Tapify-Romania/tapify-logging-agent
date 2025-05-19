a0926f6c79d9ad064a443ced2f43fcc31b32a8a8
Andrei Guzga <shhican@gmail.com>
2025-05-19 16:01:40 +0300

    More changes to how we send logs

--- setup-promtail.sh
+++ setup-promtail.sh
@@ -5,10 +5,6 @@ echo "📦 Installing Promtail..."
 
 docker pull grafana/promtail:2.9.4
 
-echo "🔧 Generating promtail-config.yml from template using HOSTNAME=${HOSTNAME}"
-export HOSTNAME=$(hostname)
-envsubst < promtail-config.template.yml > promtail-config.yml
-
 echo "🔧 Launching Promtail container..."
 
 docker compose up -d
